c73ac6f6e91064d9b398c20de0a507a29e4723bc
unknown <t.powell5589@student.leedsbeckett.ac.uk>
2017-05-01 21:57:06 +0100

    updated to WAR file

--- src/main/java/com/trp/Application.java
+++ src/main/java/com/trp/Application.java
@@ -2,13 +2,20 @@ package com.trp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-@EnableJpaAuditing
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
 }
